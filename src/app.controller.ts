69f50923f0d8f3c38acb8ff537a9bd51b21081c4
manickaperumal <manickaperumal@certaworks.in>
2023-09-19 18:01:07 +0530

    service file was created

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,51 +8,29 @@ import {
   Body,
 } from '@nestjs/common';
 import { data, ReportType } from './data';
-import { v4 as uuidv4 } from 'uuid';
+import { AppService } from './app.service';
 
 @Controller('report/:type')
 export class AppController {
+
+  constructor(
+    private readonly appService:AppService
+  ){}
   @Get()
   getAllReports(@Param('type') type: string) {
-    const reportType =
-      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    var output = data.report.filter(function (reportObj) {
-      return reportObj.type == reportType;
-    });
-    return output;
+    return this.appService.getAllReports(type)
   }
 
   @Get(':id')
   getReportById(@Param('type') type: string, @Param('id') id: string) {
-    const reportType =
-      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    var output = data.report.filter(function (reportObj) {
-      if (reportObj.type == type && reportObj.id == id) {
-        return reportObj;
-      }
-    });
-    if (output.length > 0) {
-      return output;
-    } else {
-      return 'No data found';
-    }
+    return this.appService.getReportById(type,id)
   }
 
   @Post()
   createReport(
     @Body() body: { amount: string; source: string },
     @Param('type') type: string,
-  ) {
-    const newReport = {
-      id: uuidv4(),
-      source: body.source,
-      amount: body.amount,
-      type: type == 'income' ? ReportType.INCOME : ReportType.EXPENSE,
-      created_at: new Date(),
-      updated_at: new Date(),
-    };
-    data.report.push(newReport);
-  }
+  ) {}
 
   @Put(':id')
   updateReport() {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { ReportType, data } from './data';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllReports(type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    var output = data.report.filter(function (reportObj) {
+      return reportObj.type == reportType;
+    });
+    return output;
+  }
+
+  getReportById(type: string, id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    var output = data.report.filter(function (reportObj) {
+      if (reportObj.type == type && reportObj.id == id) {
+        return reportObj;
+      }
+    });
+    if (output.length > 0) {
+      return output;
+    } else {
+      return 'No data found';
+    }
+  }
+
+  createReport(body, type: string) {
+    const newReport = {
+      id: uuidv4(),
+      source: body.source,
+      amount: body.amount,
+      type: type == 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+    data.report.push(newReport);
   }
 }
